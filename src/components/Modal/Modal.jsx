e5c4bb10bbb54b274eef967c19e20774f80a982b
YurkoKuksa <145128983+YurkoKuksa@users.noreply.github.com>
2024-01-19 21:53:06 +0100

    hooks

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -24,39 +24,3 @@ export const Modal = ({ close, children }) => {
 };
 
 export default Modal;
-
-// import React, { Component } from 'react';
-// import { ModalStyle, Overlay } from './Modal.styled';
-
-// export class Modal extends Component {
-//   componentDidMount() {
-//     document.addEventListener('keydown', handleKeyDown);
-//   }
-
-//   componentWillUnmount() {
-//     document.removeEventListener('keydown', handleKeyDown);
-//   }
-
-//   handleClickOnBackdrop = e => {
-//     console.log(e.target);
-//     if (e.currentTarget === e.target) {
-//       this.props.close();
-//     }
-//   };
-
-//   handleKeyDown = e => {
-//     if (e.key === 'Escape') {
-//       this.props.close();
-//     }
-//   };
-
-//   render() {
-//     return (
-//       <Overlay onClick={this.handleClickOnBackdrop}>
-//         <ModalStyle>{this.props.children}</ModalStyle>
-//       </Overlay>
-//     );
-//   }
-// }
-
-// export default Modal;
